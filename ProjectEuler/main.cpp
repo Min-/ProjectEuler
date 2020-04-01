2d5b9eb6a559164a6ea842197421ca554db34ff8
Min- <min_z@outlook.com>
2020-04-02 01:14:30 +0800

    init

---
+++ ProjectEuler/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ProjectEuler
+//
+//  Created by Min Zhang on 2020/4/2.
+//  Copyright © 2020 Min Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
